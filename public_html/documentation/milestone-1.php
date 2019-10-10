1bc79ebea1024b887d5054cd44d1b8e15954d89c
Timothy Beck <johndaveleet@gmail.com>
2019-10-10 15:20:50 -0600

    added some outlining and temporary goal statements to milestone-1.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,6 +5,34 @@
 	<meta charset="UTF-8">
 </head>
 <body>
-	
+	<div>
+		<h1>Purpose, Audience, Goal:</h1>
+		<br>
+		<h2>Purpose:</h2>
+
+		<br>
+		<h2>Audience:</h2>
+
+		<br>
+		<h2>Goal:</h2>
+
+		<br>
+		<br>
+		<h1>Persona:</h1>
+		<br>
+
+		<br>
+		<h1></h1>
+	</div>
+
+	<div>
+		<h2>Milestone Objectives:</h2>
+		<ul>
+			<li><s>Correctly set up your PWP using the git/GitHub/PhpStorm toolchain.</s></li>
+			<li><s>Create the correct directory structure for the project exactly as outlined below.</s></li>
+			<li>Define a purpose, audience, and goal for your project.</li>
+			<li>Create one Persona for your project.</li>
+		</ul>
+	</div>
 </body>
 </html>
