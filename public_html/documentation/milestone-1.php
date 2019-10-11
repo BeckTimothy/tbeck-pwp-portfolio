c054f2eb6665682f2dd320c4a7651fb72e8c4ed3
Timothy Beck <johndaveleet@gmail.com>
2019-10-10 19:11:46 -0600

    created a persona and added headshot of person from persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -20,18 +20,20 @@
 		<br>
 		<h1>Persona:</h1>
 		<br>
-
+		<p>Laszlo Bock, who for 10 years was the 'head of hiring' as the Senior Vice President of People Operations at Google, is a 47 year old American from Romainia, born from Hungarian parents. He graduated from Pomona College with a BA and Yale school of management with an MBA. When hiring, Bock weighs skills that relate to learning new things and solving problems as the most important skills in potential employees. He seems to be very positive and passionate about his workplace, peers, and technology. His goal is to gain more information on the potential new hire portrayed through my website.</p>
 		<br>
-		<h1></h1>
+		<div>
+			<img src="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-1/c0.0.160.160a/p160x160/37357051_10215043452773432_5609837740867715072_n.jpg?_nc_cat=100&_nc_oc=AQk95wXkztSiwwO7a3XD4bA1Nhb7FQNiui_21kqRrgLlcaU9M6lJjhC6HUUxuOyAeWU&_nc_ht=scontent-dfw5-2.xx&oh=dcf2f22b63d0d271334217eb9bfd6287&oe=5E3215A7" alt="headshot of Laszlo Bock">
+		</div>
+		<br>
 	</div>
-
 	<div>
 		<h2>Milestone Objectives:</h2>
 		<ul>
 			<li><s>Correctly set up your PWP using the git/GitHub/PhpStorm toolchain.</s></li>
 			<li><s>Create the correct directory structure for the project exactly as outlined below.</s></li>
-			<li>Define a purpose, audience, and goal for your project.</li>
-			<li>Create one Persona for your project.</li>
+			<li><s>Define a purpose, audience, and goal for your project.</s></li>
+			<li><s>Create one Persona for your project.</s></li>
 		</ul>
 	</div>
 </body>
