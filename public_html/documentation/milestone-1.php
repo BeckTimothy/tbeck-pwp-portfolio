dc95db014b58bb2e5f3b3e5ddfd79ccf9b1b409c
Timothy Beck <johndaveleet@gmail.com>
2019-10-16 13:27:36 -0600

    changed picture and persona to not be directly representative of a real person.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -20,10 +20,10 @@
 		<br>
 		<h1>Persona:</h1>
 		<br>
-		<p>Laszlo Bock, who for 10 years was the 'head of hiring' as the Senior Vice President of People Operations at Google, is a 47 year old American from Romainia, born from Hungarian parents. He graduated from Pomona College with a BA and Yale school of management with an MBA. When hiring, Bock weighs skills that relate to learning new things and solving problems as the most important skills in potential employees. He seems to be very positive and passionate about his workplace, peers, and technology. His goal is to gain more information on the potential new hire portrayed through my website.</p>
+		<p>Lando Rock, who for 12 years was the 'head of hiring' as the Senior Vice President of People Operations at Google, is a 50 year old American from Italy. He graduated from Arizona State College with a BA and Yale school of management with an MBA. When hiring, Rock weighs skills that relate to learning new things and solving problems as the most important skills in potential employees. He seems to be very positive and passionate about his workplace, peers, and technology. His goal is to gain more information on the potential new hire portrayed through my website.</p>
 		<br>
 		<div>
-			<img src="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-1/c0.0.160.160a/p160x160/37357051_10215043452773432_5609837740867715072_n.jpg?_nc_cat=100&_nc_oc=AQk95wXkztSiwwO7a3XD4bA1Nhb7FQNiui_21kqRrgLlcaU9M6lJjhC6HUUxuOyAeWU&_nc_ht=scontent-dfw5-2.xx&oh=dcf2f22b63d0d271334217eb9bfd6287&oe=5E3215A7" alt="headshot of Laszlo Bock">
+			<img src="./star-wars-lando-calrissian.jpg" alt="headshot of Lando Rock">
 		</div>
 		<br>
 	</div>
