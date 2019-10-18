454224507c8258f24ae4fb852a9c8a97b1a416c2
Timothy Beck <johndaveleet@gmail.com>
2019-10-17 22:07:31 -0600

    started my milestone-2 php doc, added wireframes and beginning html for content strategy.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>PWP Project Milestone 2</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+	<div>
+		<h1>Content Strategy</h1>
+		<p>This text will be replaced with my content strategy</p>
+		<p>for more information see the comment blocks of my html/js/css outlines</p>
+		<ul>
+			<li><a href="">index.html that contains the page layout</a></li>
+			<li><a href="">About Me, Dom inserted html</a></li>
+			<li><a href="">Contact, Dom inserted html</a></li>
+			<li><a href="">portfolio, Dom inserted html</a></li>
+			<li><a href="">resume, Dom inserted html</a></li>
+			<li><a href="">styles</a></li>
+			<li><a href="">javascript</a></li>
+		</ul>
+	</div>
+	<div>
+		<h1>Wireframes</h1>
+		<h2>Mobile</h2>
+		<p>Mobile Menu</p><img src="./wireframe-assets/Mobile-Menu.png">
+		<p>Mobile Resume</p><img src="./wireframe-assets/Resume.png">
+		<p>Mobile About</p><img src="./wireframe-assets/About.png">
+		<p>Mobile Portfolio</p><img src="./wireframe-assets/Portfolio.png">
+		<p>Mobile Contact</p><img src="./wireframe-assets/Contact.png">
+
+		<h2>Desktop</h2>
+		<p>Resume</p><img src="./wireframe-assets/Desktop-Resume.png">
+		<p>About</p><img src="./wireframe-assets/Desktop-About.png">
+		<p>Portfolio</p><img src="./wireframe-assets/Desktop-Portfolio.png">
+		<p>Contact</p><img src="./wireframe-assets/Desktop-Contact.png">
+	</div>
+</body>
+</html>
