8d640fc780ec0defa1e5db90c29c6057afc9f25e
Timothy Beck <johndaveleet@gmail.com>
2019-10-17 21:06:48 -0600

    added some html/js/css roughdrafting and wireframe pngs

---
+++ public_html/documentation/roughdrafting/scripts-roughdraft.js
@@ -0,0 +1,22 @@
+
+//create variables for each of the HTML elements we will be interacting with.
+//buttons, moving containers, dynamic containers with content
+
+//add event listeners for nav and socialmedia buttons
+
+//create callback functions for social media buttons
+const onLinkedInButtonClick = function() {};
+const onGithubButtonClick = function() {};
+const onFacebookButtonClick = function() {};
+
+//create callback functions for nav button presses
+const onResumeButtonClick = function() {};
+const onPorfolioButtonClick = function() {};
+const onAboutButtonClick = function() {};
+const onContactButtonClick = function() {};
+
+//create callback functions for mobile nav button presses
+const onResumeMobileButtonClick = function() {};
+const onPorfolioMobileButtonClick = function() {};
+const onAboutMobileButtonClick = function() {};
+const onContactMobileButtonClick = function() {};
