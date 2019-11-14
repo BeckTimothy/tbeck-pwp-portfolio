12bb84b61e4c9e8e551ee9a7c8bfef8fe6f8eda4
Timothy Beck <johndaveleet@gmail.com>
2019-11-14 16:58:13 -0700

    added comments, planning, and boilerplate for event-handler.js

---
+++ public_html/js/event-handler.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+	initEventHandling();
+};
+//remove button focus on click
+function initEventHandling() {
+	//
+}
+
+
+function switchToResume() {
+	//change button images
+	//change mobile nav text
+	//remove page content
+	//append page content
+
+}
+
+function switchToPortfolio() {
+	//change button images
+	//change mobile nav text
+	//remove page content
+	//append page content
+}
+
+function switchToContact() {
+	//change button images
+	//change mobile nav text
+	//remove page content
+	//append page content
+}
+
+function switchToAboutMe() {
+	//change button images
+	//change mobile nav text
+	//remove page content
+	//append page content
+}
