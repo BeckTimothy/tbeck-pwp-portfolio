7caeb472567a3f17b1148a5a5fd716d95096cbab
Timothy Beck <johndaveleet@gmail.com>
2019-11-13 22:02:09 -0700

    created contact.php psuedocode.

--- public_html/documentation/roughdrafting/content-to-append/contact.php
+++ public_html/documentation/roughdrafting/content-to-append/contact.php
@@ -1,1 +1,51 @@
 <?php
+/**
+ * Constructor to create an Email for contacting Timothy Beck
+ *
+ * @param string $newYourName
+ * @param string $newYourEmail
+ * @param string $newYourEmailSubject
+ * @param string $newYourEmailBody
+ * @param boolean $newYourEmailVerification
+ * @throws \InvalidArgumentException if inputs are empty or Invalid
+ * @throws \RangeException if input data is too long
+ * @throws \TypeError if data types violate type hints
+ */
+public function __construct(string $newYourName,
+									 string $newYourEmail,
+									 string $newYourEmailSubject,
+									 string $newYourEmailBody,
+									 boolean $newYourEmailVerification) {
+	try {
+		$this->setYourName($newYourName);
+
+
+
+		$this->setYourEmail($newYourEmail);
+
+
+
+		$this->setYourEmailSubject($newYourEmailSubject);
+
+
+
+		$this->setYourEmailBody($newYourEmailBody);
+
+
+
+
+
+
+
+		$this->setYourEmailVerification($newYourEmailVerification);
+
+
+
+
+	} // determine if/what exception was thrown
+	catch(\InvalidArgumentException | \RangeException | \TypeError | \Exception $exception) {
+		$exceptionType = get_class($exception);
+		throw(new $exceptionType($exception->getMessage(), 0, $exception));
+	}
+}
+?>
