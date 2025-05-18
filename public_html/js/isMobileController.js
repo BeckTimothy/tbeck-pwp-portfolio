2e07bad87d0e9296c452d8e376bbd3f3996e6cab
Timothy Beck <dev@timothybeck.com>
2025-05-18 16:30:56 -0600

    mobile responsiveness

--- public_html/js/isMobileController.js
+++ public_html/js/isMobileController.js
@@ -1,8 +1,4 @@
-
-
-
-
-if (window.screen.width < 1000 || window.visualViewport.width < 1000){
+if (window.screen.width < 1100 || window.visualViewport.width < 1100){
     let webringPrevLinkElement = document.getElementById("webringPrevLink");
     let webringCenterLinkElement = document.getElementById("webringCenterLink");
     let webringNextLinkElement = document.getElementById("webringNextLink");
@@ -11,3 +7,19 @@ if (window.screen.width < 1000 || window.visualViewport.width < 1000){
     webringCenterLinkElement.innerText=" ALA! ";
     webringNextLinkElement.innerText= ">";
 }
+
+
+window.addEventListener("resize", function(event) {
+    let webringPrevLinkElement = document.getElementById("webringPrevLink");
+    let webringCenterLinkElement = document.getElementById("webringCenterLink");
+    let webringNextLinkElement = document.getElementById("webringNextLink");
+    if (window.screen.width < 1100 || window.visualViewport.width < 1100){
+        webringPrevLinkElement.innerText="<";
+        webringCenterLinkElement.innerText=" ALA! ";
+        webringNextLinkElement.innerText= ">";
+    }else{
+        webringPrevLinkElement.innerText="<< prev";
+        webringCenterLinkElement.innerText="Lowtechs Anonymous Webring";
+        webringNextLinkElement.innerText= "next >>";
+    }
+});
