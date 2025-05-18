bac3f966ce30eab21dc4f85be122681ee9ecd5b8
Timothy Beck <dev@timothybeck.com>
2025-05-18 11:47:14 -0600

    made webring look better for mobile despite entire site not being mobile friendly

---
+++ public_html/js/isMobileController.js
@@ -0,0 +1,13 @@
+
+
+
+
+if (window.screen.width < 1000){
+    let webringPrevLinkElement = document.getElementById("webringPrevLink");
+    let webringCenterLinkElement = document.getElementById("webringCenterLink");
+    let webringNextLinkElement = document.getElementById("webringNextLink");
+
+    webringPrevLinkElement.innerText="<";
+    webringCenterLinkElement.innerText=" ALA! ";
+    webringNextLinkElement.innerText= ">";
+}
